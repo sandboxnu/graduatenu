92ce35267755fba736ff4aa6db2df41218f88482
meebs <alex@takayamasan.com>
2019-06-12 21:40:04 -0400

    added initial files from old repository.

---
+++ parse_degree_audit.js
@@ -0,0 +1,139 @@
+const fs = require('fs');
+
+fs.readFile('./Web\ Audit.html', 'utf8', (err, data) => {
+	if(err) {throw err;}
+	// else, process the data
+
+	json = { 
+		completed: {
+			classes:[],
+			nupaths:[]
+		},
+		inprogress: {
+			classes:[],
+			nupaths:[]
+		},
+		requirements: {
+			classes:[],
+			nupaths:[]
+		},
+		data: {
+		
+		}
+	};
+
+	var lines = data.split('\n');
+	for(var i = 0; i < lines.length; i++) {
+
+		// finds major
+		if(contains(lines[i], 'Major')) {
+			json.data.major = lines[i].substring(lines[i].search('>') + 1, lines[i].search(' - Major'));
+		}
+		// finds year
+		if(contains(lines[i], 'CATALOG YEAR')) {
+			json.data.year = lines[i].substring(lines[i].search('CATALOG YEAR:') + 'CATALOG YEAR: '.length, lines[i].search('CATALOG YEAR:') + 'CATALOG YEAR: '.length + 4);
+		}
+
+		if(contains(lines[i], 'GRADUATION DATE:')) {
+			json.data.grad = lines[i].substring(lines[i].search('GRADUATION DATE: ') + 'GRADUATION DATE: '.length, lines[i].search('GRADUATION DATE: ') + 'GRADUATION DATE:  '.length + 7);
+		
+		}
+
+		// finds all of the nupaths
+		if(contains(lines[i], 'No course taken pass/fail can be used toward NUpath.'))	{
+			i++;
+			while(contains(lines[i], '<br>')) {
+				var toAdd = lines[i].substring(lines[i].indexOf("(") + 1, lines[i].indexOf("(") + 3)
+				// omits bad stuff
+				if(contains(toAdd, '<') || contains(toAdd, '>')) {
+				}
+
+				else if(contains(lines[i], 'OK')|| contains(lines[i], 'IP')) {
+					json.completed.nupaths.push(toAdd);				
+				}
+				else if(contains(lines[i], 'IP')) {
+					json.inprogress.nupaths.push(toAdd);
+				}
+				else if(contains(lines[i], 'NO')) {
+					json.requirements.nupaths.push(toAdd);				
+				}
+				i++;
+			}
+		}
+
+
+		// this part doesn't work! ends up in infinite loop ?? 
+		else if(contains(lines[i], 'Course List')) {
+			var courseList = lines[i].substring(lines[i].search('Course List') + 13).replace('<font>', '').split('<font class="auditPreviewText">').join('').split('</font>').join('TO');
+			var type = '';	
+			for(var i = 0; i < courseList.length; i++) {
+
+				var course = { };
+
+				if(courseList[i] == ' ' || courseList[i] == '') {
+
+				}
+
+				else if(contains(courseList[i], '\w\w')) {
+					course.attr = courseList[i].substring(0, 5); 
+					course.number = courseList[i].substring(5, 10);	
+					json.requirements.classes.push(course);
+				}
+
+				else {
+					course.attr = type; 
+					course.number = courseList[i];
+					json.requirements.classes.push(course);
+				}
+			}
+
+			console.log(courseList);
+		}
+
+		//	else if(contains(lines[i], '<span class="auditPreviewText"><font class="auditPreviewText">       NUpath                                               ')) {
+
+		// finds nupaths registered and adds them to course
+
+		// get identifiable course info such as number
+		// search completed courses for course
+		// add nupath header associated with that course to that course;
+
+		// this information can be filled in by pulling stuff from the web
+		//		console.log("this is nupath stuff lol");
+		// skip this stuff to avoid duplicate courses
+		//		i+=9000;
+		//	}
+
+		// finds all courses taken, currently taking or scheduled to take
+		else if(contains(lines[i], 'FL') || contains(lines[i], 'SP') ||contains(lines[i], 'S1') || contains(lines[i], 'S2')) {
+			//TODO: this is hacky help
+			var course = {};
+			var courseString = lines[i].substring(lines[i].search('(FL|SP|S1|S2)'));
+			course.hon = contains(lines[i], '\(HON\)');
+			
+			// ap courses that do not count for credit do not have numbers / attributes for corresponding college courses
+			if(!contains(courseString, 'NO AP')) {
+				course.attr = lines[i].substring(lines[i].search('(FL|SP|S1|S2)') + 5, lines[i].search('(FL|SP|S1|S2)') + 9).replace(' ', '');
+				course.number = courseString.substring(9, 14);
+			}
+			course.credithours = courseString.substring(18, 22);
+			course.season = new RegExp('(FL|SP|S1|S2)').exec(lines[i])[0];
+			course.year = lines[i].substring(lines[i].search('(FL|SP|S1|S2)') + 2, lines[i].search('(FL|SP|S1|S2)') + 4);
+			
+			if(contains(courseString, 'IP')) {
+				json.inprogress.classes.push(course);			
+			} else {
+				json.completed.classes.push(course);
+			}
+		}
+	}
+
+	console.log(json);
+	
+	fs.writeFileSync('parsed_audit.json', JSON.stringify(json));
+});
+
+function contains(text, lookfor) {
+	return -1 != text.search(lookfor);
+}
+
---
+++ parser.js
@@ -0,0 +1,99 @@
+/*
+JSON Schedule Formatter for ScheduleNEU
+Alex Takayama
+
+Converts JSON schedule data to a JSON formatted schedule.
+
+Reads JSON data in from INPUT.
+writes JSON schedule data to OUTPUT.
+
+Relies on 'parser_funcs.js'
+*/
+
+// input and output paths.
+const INPUT = './sampleData.json';
+const OUTPUT = './schedule.json';
+
+// parsing functions
+const parsing = require('./parser_funcs.js');
+// file reading
+const fs = require('fs');
+
+// main method
+var main = (inputLocation, outputLocation) => {
+  // grab the json file
+  var data;
+  try {
+    data = fs.readFileSync(inputLocation);
+  } catch (err) {
+    console.log("bad file path");
+    throw new Error("Error trying to read .json file. bad file path.")
+    // return;
+  }
+
+  // parse the json file
+  let old = JSON.parse(data);
+
+  // log the things in the json file
+  /*
+  console.log(old.completed);
+  console.log(old.requirements);
+  console.log(old.otherInfo);
+  */
+
+  // get the completed, requirements, and otherInfo
+  let completed = old.completed;
+  let requirements = old.requirements;
+  let otherInfo = old.otherInfo;
+
+  let completedClasses = completed.classes;
+
+  // seasons constant
+  const seasons = ["SP", "S1", "S2", "FL"];
+
+  // START SORTING
+
+  // start at the lowest year
+  let currentYear = parsing.getFirstYear(completedClasses);
+  let schedule = [];
+
+  // while we have a next year, append the year to a schedule.
+  while (currentYear) {
+    schedule.push(parsing.makeYear(currentYear, seasons, completedClasses));
+    currentYear = parsing.getNextYear(currentYear, completedClasses);
+  }
+
+  // output the file to ./schedule.json.
+  fs.writeFile(outputLocation, JSON.stringify(schedule, null, 2), (err) => {
+    if (err) throw err;
+    console.log("success.");
+  });
+
+  // somehow convert the rest of the stuff to a schedule. complete with coop cycles? 
+  // need to somehow build a tree of the requirements for classes, and then grab from the bottom. 
+  // TODO : convert 'requirements' into the rest of the schedule.
+
+  /*
+  POTENTIAL CRITERIAS for building schedule:
+
+  coop cycle (spring/fall/none?)
+  option for other requirements/objectives (minors)
+  when classes are available
+  prerequisits for classes
+
+  ADD-ONS:
+
+  "difficulty" of classes (to balance workload)
+  suggest classes that overlap with requirements (NUPATH, major, etc.)
+
+
+  INFORMATION NEEDED: (whether scraped, or from SearchNEU)
+
+  when classes are available
+  prerequisites for classes
+  "difficulty" of classes - from trace? or by survey
+  */
+}
+
+// run the main program. 
+main(INPUT, OUTPUT);
