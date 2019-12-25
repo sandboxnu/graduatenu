d70354f40480f7ec049a66b8063175e7c62eb09f
Yash J Shetty <yash17shetty@gmail.com>
2019-12-25 08:54:25 +0530

    ys - note on majorIDs

--- frontend/src/majors/index.ts
+++ frontend/src/majors/index.ts
@@ -1,3 +1,5 @@
+//Note: major ids for new majors should just be added here for now altough this is not ideal.
+//In the future we should probably be able to fetch a list of all major ids from the search backend.
 export const majorIds = [
   "computer-information-science/computer-science/bscs",
   "science/biochemistry/biochemistry-bs",
