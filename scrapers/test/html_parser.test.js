e1678ea86a26c1dc28fe8e96e8da645940cf5d0b
Yash J Shetty <yash17shetty@gmail.com>
2020-02-22 23:49:46 -0500

    fix failing scraper tests

--- scrapers/test/html_parser.test.js
+++ scrapers/test/html_parser.test.js
@@ -2,19 +2,22 @@ const html_parser = require("../src/html_parser.ts");
 const fs = require("fs");
 
 const cs_json = html_parser.audit_to_json(
-  fs.readFileSync("./backend/test/mock_audits/cs_audit.html", "utf-8")
+  fs.readFileSync("./scrapers/test/mock_audits/cs_audit.html", "utf-8")
 );
 const cs_json2 = html_parser.audit_to_json(
-  fs.readFileSync("./backend/test/mock_audits/cs_audit2.html", "utf-8")
+  fs.readFileSync("./scrapers/test/mock_audits/cs_audit2.html", "utf-8")
 );
 const cs_json3 = html_parser.audit_to_json(
-  fs.readFileSync("./backend/test/mock_audits/cs_audit3.html", "utf-8")
+  fs.readFileSync("./scrapers/test/mock_audits/cs_audit3.html", "utf-8")
 );
 const cs_math_json = html_parser.audit_to_json(
-  fs.readFileSync("./backend/test/mock_audits/cs_math_grad_audit.html", "utf-8")
+  fs.readFileSync(
+    "./scrapers/test/mock_audits/cs_math_grad_audit.html",
+    "utf-8"
+  )
 );
 const me_json = html_parser.audit_to_json(
-  fs.readFileSync("./backend/test/mock_audits/me_audit.html", "utf-8")
+  fs.readFileSync("./scrapers/test/mock_audits/me_audit.html", "utf-8")
 );
 
 const json_ex = [];
