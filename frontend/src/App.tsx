e53fce515533dc385f1b6d8892efa2d4a22587a4
imanmoreira <moreira.i@husky.neu.edu>
2020-04-01 12:37:43 -0400

    removed unsused screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Onboarding } from "./Onboarding/Onboarding";
 import { HomeWrapper } from "./home/HomeWrapper";
-import { NameScreen } from "./Onboarding/NameScreen";
-import { AcademicYearScreen } from "./Onboarding/AcademicYearScreen";
-import { GraduationYearScreen } from "./Onboarding/GraduationYearScreen";
 import { OnboardingInfoScreen } from "./Onboarding/OnboardingInfoScreen";
-import { MajorScreen } from "./Onboarding/MajorScreen";
 import { Provider } from "react-redux";
 import { Store } from "redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -25,10 +21,6 @@ export const App = ({
         <Router>
           <Switch>
             <Route path="/home" component={HomeWrapper} />
-            <Route path="/name" component={NameScreen} />
-            <Route path="/academicYear" component={AcademicYearScreen} />
-            <Route path="/graduationYear" component={GraduationYearScreen} />
-            <Route path="/major" component={MajorScreen} />
             <Route path="/onboarding" component={OnboardingInfoScreen} />
             <Route path="/" component={Onboarding} />
           </Switch>
