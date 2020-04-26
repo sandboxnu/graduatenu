c9ce46ec9ef9ff7d5291cb557f25426b0a15eaf2
Stanley Liu <liustan24@gmail.com>
2020-04-26 12:56:11 -0400

    for merge conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Onboarding } from "./Onboarding/Onboarding";
 import { HomeWrapper } from "./home/HomeWrapper";
-<<<<<<< HEAD
 import { OnboardingInfoScreen } from "./Onboarding/OnboardingInfoScreen";
-=======
-import { NameScreen } from "./Onboarding/NameScreen";
-import { AcademicYearScreen } from "./Onboarding/AcademicYearScreen";
-import { GraduationYearScreen } from "./Onboarding/GraduationYearScreen";
-import { MajorScreen } from "./Onboarding/MajorScreen";
-import { SignupScreenComponent } from "./Onboarding/SignupScreen";
->>>>>>> basic signup ui
 import { Provider } from "react-redux";
 import { Store } from "redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -29,15 +21,7 @@ export const App = ({
         <Router>
           <Switch>
             <Route path="/home" component={HomeWrapper} />
-<<<<<<< HEAD
             <Route path="/onboarding" component={OnboardingInfoScreen} />
-=======
-            <Route path="/name" component={NameScreen} />
-            <Route path="/academicYear" component={AcademicYearScreen} />
-            <Route path="/graduationYear" component={GraduationYearScreen} />
-            <Route path="/major" component={MajorScreen} />
-            <Route path="/signup" component={SignupScreenComponent} />
->>>>>>> basic signup ui
             <Route path="/" component={Onboarding} />
           </Switch>
         </Router>
