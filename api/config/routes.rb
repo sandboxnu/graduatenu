8b063727aeca8690b1d692fe9c58625df61f603b
Yash J Shetty <yash17shetty@gmail.com>
2020-05-07 20:11:28 -0400

    ys - add healthcheck endpoint

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,5 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  Healthcheck.routes(self)
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
 
