5cc8b11b77700bcf422056f65d0ca4ba7d624832
imanmoreira <moreira.i@husky.neu.edu>
2020-05-08 03:08:28 -0400

    8 ;-;

--- frontend/src/Onboarding/CompletedCoursesScreen.tsx
+++ frontend/src/Onboarding/CompletedCoursesScreen.tsx
@@ -55,7 +55,7 @@ const CourseText = styled.p`
   font-weight: 400;
 `;
 
-const ScrollWrapper = styled.div`
+const ScrollWrapper = styled.body`
   &::-webkit-scrollbar {
     -webkit-appearance: none;
     width: 7px;
