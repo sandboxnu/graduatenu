d4529072b790db14d51d8765b11fb6993edb96d4
Yash J Shetty <yash17shetty@gmail.com>
2020-05-07 20:10:22 -0400

    ys - move db:create & db:migrate to container entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /api/tmp/pids/server.pid
 
+rails db:create
+rails db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
