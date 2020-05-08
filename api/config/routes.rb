c1e78bc439e2c3e06a357e768fb5f850dd529579
Yash J Shetty <yash17shetty@gmail.com>
2020-05-07 20:29:25 -0400

    ys - update healthcheck route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  Healthcheck.routes(self)
   scope :api, defaults: { format: :json } do
+    Healthcheck.routes(self)
     devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
 
     resources :users, only: [:show, :update] do
