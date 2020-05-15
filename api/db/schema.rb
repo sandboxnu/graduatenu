890643acae19fc337a8b1d4dbbc4baa1de860f55
Stanley Liu <liustan24@gmail.com>
2020-05-15 12:41:14 -0400

    added major strings to plans backend and updated gitignore

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_005447) do
+ActiveRecord::Schema.define(version: 2020_05_15_163636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2020_03_12_005447) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "major"
+    t.string "planString"
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
