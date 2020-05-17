e4810ca9a9dc24c3f978ea9ff7a9cce107fb4e56
nathan <cunningham.wil@husky.neu.edu>
2020-05-17 15:41:02 -0400

    regex for spaces in usernames

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :validatable
-  
-  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  #, uniqueness:
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, allow_blank: false, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
   
   def generate_jwt
     JWT.encode({ id: id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
