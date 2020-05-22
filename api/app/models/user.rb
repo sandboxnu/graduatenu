ca7315e0c427ed65b84ca458d697eb16769f1980
William Cunningham <cunningham.wil@husky.neu.edu>
2020-05-22 12:59:19 -0400

    fixed formatting and added comment

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :validatable
-  #, uniqueness:
+  
+  #validates a non-unique username and allows spaces
   validates :username, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
   
   def generate_jwt
