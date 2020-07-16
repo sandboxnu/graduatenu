50d03db4c0cd40d185567f9c369686d2624829b8
Nils Backe <nils@bluefintechpartners.com>
2020-07-15 17:22:15 -0700

    move semester type to its own file

--- frontend/src/components/Year/YearTop.tsx
+++ frontend/src/components/Year/YearTop.tsx
@@ -1,56 +1,11 @@
 import * as React from "react";
 import styled from "styled-components";
-import { MenuItem } from "@material-ui/core";
 import { SeasonWord, Status } from "graduate-common";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { changeSemesterStatusAction } from "../../state/actions/scheduleActions";
 import { AppState } from "../../state/reducers/state";
-import Select from "@material-ui/core/Select";
-import { withStyles } from "@material-ui/core/styles";
-import InputBase from "@material-ui/core/InputBase";
-
-const SemesterType = withStyles(theme => ({
-  root: {
-    color: "white",
-    marginTop: "2px",
-    marginLeft: "4px",
-  },
-  icon: {
-    color: "white",
-    marginTop: "1px",
-  },
-}))(Select);
-
-const SemesterTypeInput = withStyles(theme => ({
-  root: {
-    color: "#FFFFFF",
-  },
-  input: {
-    color: "#FFFFFF",
-    position: "relative",
-    fontSize: 16,
-    transition: theme.transitions.create(["border-color", "box-shadow"]),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "&:focus": {
-      borderRadius: 4,
-      borderColor: "#80bdff",
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
-    },
-  },
-}))(InputBase);
+import { SemesterType } from "./SemesterType";
 
 const Container = styled.div`
   display: flex;
@@ -177,54 +132,30 @@ class YearTopComponent extends React.Component<Props, YearTopState> {
             <span style={{ fontWeight: "normal" }}> - </span>
           </SemesterText>
           <SemesterType
-            id="simple-menu"
-            value={this.state.fallStatus}
-            input={<SemesterTypeInput />}
+            status={this.state.fallStatus}
             onChange={(event: any) => this.handleChange(event, "fall")}
-          >
-            <MenuItem value={"CLASSES"}>Classes</MenuItem>
-            <MenuItem value={"COOP"}>Co-op</MenuItem>
-            <MenuItem value={"INACTIVE"}>Vacation</MenuItem>
-          </SemesterType>
+          />
         </div>
         <div style={textContainerStyle}>
           <SemesterText>Spring {year + 1} - </SemesterText>
           <SemesterType
-            id="simple-menu"
-            value={this.state.springStatus}
-            input={<SemesterTypeInput />}
+            status={this.state.springStatus}
             onChange={(event: any) => this.handleChange(event, "spring")}
-          >
-            <MenuItem value={"CLASSES"}>Classes</MenuItem>
-            <MenuItem value={"COOP"}>Co-op</MenuItem>
-            <MenuItem value={"INACTIVE"}>Vacation</MenuItem>
-          </SemesterType>
+          />
         </div>
         <div style={textContainerStyle}>
           <SemesterText>Summer I {year + 1} - </SemesterText>
           <SemesterType
-            id="simple-menu"
-            value={this.state.summer1Status}
-            input={<SemesterTypeInput />}
+            status={this.state.summer1Status}
             onChange={(event: any) => this.handleChange(event, "summer1")}
-          >
-            <MenuItem value={"CLASSES"}>Classes</MenuItem>
-            <MenuItem value={"COOP"}>Co-op</MenuItem>
-            <MenuItem value={"INACTIVE"}>Vacation</MenuItem>
-          </SemesterType>
+          />
         </div>
         <div style={textContainerStyle}>
           <SemesterText>Summer II {year + 1} - </SemesterText>
           <SemesterType
-            id="simple-menu"
-            value={this.state.summer2Status}
-            input={<SemesterTypeInput />}
+            status={this.state.summer2Status}
             onChange={(event: any) => this.handleChange(event, "summer2")}
-          >
-            <MenuItem value={"CLASSES"}>Classes</MenuItem>
-            <MenuItem value={"COOP"}>Co-op</MenuItem>
-            <MenuItem value={"INACTIVE"}>Vacation</MenuItem>
-          </SemesterType>
+          />
         </div>
       </Container>
     );
