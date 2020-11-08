96d6854b3a767364c5a35faca5fb3f56452a9161
Andrew Leung <andrewleung104@gmail.com>
2020-11-08 14:47:51 -0500

    add languages to ib exams

--- common/ib_exams.ts
+++ common/ib_exams.ts
@@ -304,6 +304,527 @@ export const IBExamGroups2020To2021: Array<TransferableExamGroup> = [
         ],
         semesterHours: 8,
       },
+      {
+        name: "Arabic A: Literature OR Arabic A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "ARAB",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Arabic B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "ARAB",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "ARAB",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Azerbaijani A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Bengali A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Burmese A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Chinese A: Literature OR Chinese A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "CHNS",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Chinese B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "CHNS",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "CHNS",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Danish A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Dutch A: Literature OR Dutch A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Filipino A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Finnish A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "French A: Literature OR French A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FRNH",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "French B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "FRNH",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "FRNH",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "German A: Literature OR German A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "GRMN",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "German B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "GRMN",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "GRMN",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Hindi A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Hungarian A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Indonesian A: Literature OR Indonesian A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Indonesian B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Italian A: Literature OR Italian A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "ITLN",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Italian B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "ITLN",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "ITLN",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Japanese A: Literature OR Japanese A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "JPNS",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Japanese B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "JPNS",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "JPNS",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Korean A: Literature OR Korean A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Latin",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Latvian A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Mandarin B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "CHNS",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name:
+          "Modern Greek A: Literature OR Modern Greek A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Norwegian A: Literature OR Norwegian A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Persian A: Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Portuguese A: Literature OR Portuguese A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "PORT",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Portuguese B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "PORT",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Russian A: Literature OR Russian A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "RSSN",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Russian B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "RSSN",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "RSSN",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Spanish A: Literature OR Spanish A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "SPNS",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Spanish B",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1101,
+            subject: "SPNS",
+          },
+          {
+            type: "COURSE",
+            classId: 1102,
+            subject: "SPNS",
+          },
+        ],
+        semesterHours: 8,
+      },
+      {
+        name: "Swedish A: Literature OR Swedish A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Thai A: Literature OR Thai A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Turkish A: Literature OR Turkish A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
+      {
+        name: "Vietnamese A: Literature OR Vietnamese A: Language & Literature",
+        type: "IB",
+        mappableCourses: [
+          {
+            type: "COURSE",
+            classId: 1990,
+            subject: "FLNG",
+          },
+        ],
+        semesterHours: 4,
+      },
     ],
   },
   {
