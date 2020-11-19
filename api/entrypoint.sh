b1112601fb1d6f15656dd41c6d278af12ac63e20
Nils Backe <nils.backe@gmail.com>
2020-11-19 02:05:12 -0500

    it works

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /api/tmp/pids/server.pid
+rm -f /app/tmp/pids/server.pid
 
-rails db:create
-rails db:migrate
+rails db:migrate 2>/dev/null || rails db:setup
+rails s -p 80 -b '0.0.0.0'
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
