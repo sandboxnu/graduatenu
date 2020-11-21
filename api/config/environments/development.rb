1bc45071fa860aa738ca6a171106d1496b9e51f7
Nils Backe <nils.backe@gmail.com>
2020-11-20 22:44:30 -0500

    final touches

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
-  config.hosts << 'localhost'
-  config.hosts << '127.0.0.1'
+  config.hosts.clear
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- api/test/models/plan_test.rb
+++ api/test/models/plan_test.rb
@@ -2,19 +2,18 @@
 #
 # Table name: plans
 #
-#  id                        :bigint(8)        not null, primary key
-#  course_counter            :integer(4)
-#  course_warnings           :json             default([]), is an Array
-#  is_currently_being_edited :boolean          default(FALSE), not null
-#  link_sharing_enabled      :boolean
-#  major                     :string
-#  name                      :string
-#  planString                :string
-#  schedule                  :json
-#  warnings                  :json             default([]), is an Array
-#  created_at                :datetime         not null
-#  updated_at                :datetime         not null
-#  user_id                   :bigint(8)        not null
+#  id                   :bigint(8)        not null, primary key
+#  coop_cycle           :string
+#  course_counter       :integer(4)
+#  course_warnings      :json             default([]), is an Array
+#  link_sharing_enabled :boolean
+#  major                :string
+#  name                 :string
+#  schedule             :json
+#  warnings             :json             default([]), is an Array
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  user_id              :bigint(8)        not null
 #
 # Indexes
 #
