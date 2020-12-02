f27056d5c7065553e18a2ee31eb44ac20e93049c
Arun Jeevanantham <arun@aruns-mbp.home>
2020-12-02 11:02:00 -0500

    drop courses

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_221324) do
+ActiveRecord::Schema.define(version: 2020_12_02_155819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "courses", force: :cascade do |t|
-    t.integer "class_id", null: false
-    t.string "subject", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_courses_on_user_id"
-  end
 
   create_table "plans", force: :cascade do |t|
     t.string "name"
