6521f16de42ffb0f1a5b29fc76cbc7c6cc0ae3fe
Nils Backe <nils.backe@gmail.com>
2020-12-03 22:38:13 -0500

    refactor profile screen to a FC

--- frontend/src/profile/Profile.tsx
+++ frontend/src/profile/Profile.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { withRouter, Link } from "react-router-dom";
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { OutlinedButton } from "../components/common/OutlinedButton";
 import { PrimaryButton } from "../components/common/PrimaryButton";
 import { TextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
@@ -13,24 +12,19 @@ import {
   setUserMajorAction,
 } from "../state/actions/userActions";
 import {
-  getUserCoopCycleFromState,
-  getUserMajorFromState,
   getMajorsFromState,
-  getUserIdFromState,
-  getUserPlansFromState,
+  getUserFromState,
+  getPlansFromState,
 } from "../state";
-import { Dispatch } from "redux";
-import { Major, Schedule } from "../../../common/types";
+import { Schedule } from "../../../common/types";
 import { AppState } from "../state/reducers/state";
 import { planToString } from "../utils";
 import { updateUser } from "../services/UserService";
 import { IUpdateUser, IUpdateUserData } from "../models/types";
-import { findMajorFromName } from "../utils/plan-helpers";
-import { ChangePasswordModal } from "./ChangePasswordModal";
 import { getAuthToken } from "../utils/auth-helpers";
 
 const OuterContainer = styled.div`
-  width: 50%;
+  width: 70%;
   margin: 0 auto;
 `;
 
@@ -40,7 +34,7 @@ const InnerContainer = styled.div`
   border: 3px solid #eb5757;
   box-sizing: border-box;
   margin: 10%;
-  padding: 30px 20% 40px 30px;
+  padding: 30px;
 `;
 
 const ProfileTitle = styled.h1`
@@ -55,12 +49,13 @@ const ProfileTitleContainer = styled.div`
 const DataContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
 `;
 
 const ProfileColumn = styled.div`
   display: flex;
   flex-direction: column;
+  flex: 1;
+  padding: 0px 12px 0px 12px;
 `;
 
 const ProfileEntryContainer = styled.div`
@@ -92,218 +87,157 @@ const StyledLink = styled(Link)`
   padding: 20px 0px 0px 0px;
   color: #eb5757;
 `;
-interface ProfileNameProps {
-  name: string;
-  isEdit: boolean;
-  setName: (name: string) => void;
-}
 
-interface ProfileMajorProps {
-  major: string;
-  isEdit: boolean;
-  majors: Major[];
-  setMajor: (major: Major | undefined) => void;
-}
+const ProfileComponent: React.FC = () => {
+  const dispatch = useDispatch();
+  const { user, majors, plans } = useSelector((state: AppState) => ({
+    user: getUserFromState(state), // best to update this screen when any part of the user changes
+    majors: getMajorsFromState(state),
+    plans: getPlansFromState(state),
+  }));
 
-interface ProfileCoopProps {
-  major: Major;
-  coop: string;
-  isEdit: boolean;
-  plans: Record<string, Schedule[]>;
-  setCoop: (coop: Schedule | undefined) => void;
-}
-
-interface ProfileEmailProps {
-  email: string;
-  isEdit: boolean;
-  setEmail: (email: string) => void;
-}
-
-interface SaveProps {
-  setEdit: Function;
-  name: string;
-  email: string;
-  major: Major;
-  coop: string;
-  id: number;
-  setDeclaredMajorAction: (major?: Major) => void;
-  setUserCoopCycleAction: (plan: string) => void;
-  setFullNameAction: (fullName: string) => void;
-  setEmailAction: (email: string) => void;
-}
-
-interface ChangePasswordProps {
-  id: number;
-}
-
-// const ProfileName = (props: ProfileNameProps) => {
-//   return (
-//     <ProfileEntryContainer>
-//       <ItemTitle> Name </ItemTitle>
-//       {props.isEdit && (
-//         <TextField
-//           id="outlined-basic"
-//           variant="outlined"
-//           value={props.name}
-//           onChange={e => props.setName(e.target.value)}
-//           placeholder="John Smith"
-//         />
-//       )}
-//       {!props.isEdit && <ItemEntry> {props.name} </ItemEntry>}
-//     </ProfileEntryContainer>
-//   );
-// };
-
-const ProfileMajor = (props: ProfileMajorProps) => {
-  const val = props.major != "" ? props.major : "None Selected";
-  return (
-    <ProfileEntryContainer>
-      <ItemTitle> Major </ItemTitle>
-      {props.isEdit && (
-        <Autocomplete
-          disableListWrap
-          options={props.majors.map((maj: { name: any }) => maj.name)}
-          renderInput={params => (
-            <TextField {...params} variant="outlined" fullWidth />
-          )}
-          value={val}
-          onChange={(event: React.SyntheticEvent<{}>, value: any) =>
-            props.setMajor(findMajorFromName(value, props.majors))
-          }
-        />
-      )}
-      {!props.isEdit && <ItemEntry> {val} </ItemEntry>}
-    </ProfileEntryContainer>
-  );
-};
-
-const ProfileCoop = (props: ProfileCoopProps) => {
-  const val = props.coop != "" ? props.coop : "None Selected";
-  return (
-    <ProfileEntryContainer>
-      <ItemTitle> Co-op Cycle </ItemTitle>
-      {props.isEdit && (
-        <Autocomplete
-          disableListWrap
-          options={props.plans[props.major.name].map((p: Schedule) =>
-            planToString(p)
-          )}
-          renderInput={params => (
-            <TextField {...params} variant="outlined" fullWidth />
-          )}
-          value={val}
-          onChange={(event: React.SyntheticEvent<{}>, value: any) =>
-            props.setCoop(value)
-          }
-        />
-      )}
-      {!props.isEdit && <ItemEntry> {val} </ItemEntry>}
-    </ProfileEntryContainer>
-  );
-};
-
-/*
-TODO: // Add Advsisors to profile page once we support them
-const ProfileAdvisor = (props: any) => {
-    return (
-        <ProfileEntryContainer>
-            <ItemTitle> Advisor </ItemTitle>
-            {props.isEdit &&
-                <Autocomplete
-                    disableListWrap
-                    options={["advisor", "advisor 2", "advisor 3"]}
-                    renderInput={params => (
-                    <TextField
-                        {...params}
-                        variant="outlined"
-                        fullWidth
-                    />
-                    )}
-                    value={props.advisor}
-                    onChange={(event: React.SyntheticEvent<{}>, value: any) => props.setAdvisor(value)}
-                />
-            }
-            {!props.isEdit &&
-                <ItemEntry> {props.advisor} </ItemEntry>
-            }
-        </ProfileEntryContainer>
-    );
-}
-*/
-
-// const ProfileEmail = (props: ProfileEmailProps) => {
-//   return (
-//     <ProfileEntryContainer>
-//       <ItemTitle> Email </ItemTitle>
-//       {props.isEdit && (
-//         <TextField
-//           id="outlined-basic"
-//           variant="outlined"
-//           value={props.email}
-//           onChange={e => props.setEmail(e.target.value)}
-//           placeholder="johnsmith@email.com"
-//         />
-//       )}
-//       {!props.isEdit && <ItemEntry> {props.email} </ItemEntry>}
-//     </ProfileEntryContainer>
-//   );
-// };
-
-const save = (props: SaveProps) => {
-  props.setEdit(false);
-  props.setDeclaredMajorAction(props.major);
-  props.setFullNameAction(props.name);
-  props.setEmailAction(props.email);
-  props.setUserCoopCycleAction(props.coop);
-
-  const token = getAuthToken();
-
-  const user: IUpdateUser = {
-    token: token,
-    id: props.id,
-  };
-
-  const updateUserData: IUpdateUserData = {
-    username: props.name,
-    email: props.email,
-    major: props.major != undefined ? props.major.name : "",
-    coop_cycle:
-      props.coop != undefined && props.coop !== "None Selected"
-        ? props.coop
-        : "",
-  };
-  updateUser(user, updateUserData);
-};
-
-const SaveButton = (props: SaveProps) => {
-  return <PrimaryButton onClick={() => save(props)}>Save</PrimaryButton>;
-};
-
-const ChangePassword = (props: ChangePasswordProps) => {
-  const [open, setOpen] = React.useState(false);
-  const token = getAuthToken();
-
-  return (
-    <div>
-      <ProfileEntryContainer>
-        <ItemTitle> Password </ItemTitle>
-        <div onClick={() => setOpen(true)}>
-          <OutlinedButton>Change Password</OutlinedButton>
-        </div>
-      </ProfileEntryContainer>
-      <ChangePasswordModal open={open} setOpen={setOpen} token={token} />
-    </div>
-  );
-};
-
-const ProfileComponent: React.FC = (props: any) => {
   const [isEdit, setEdit] = useState(false);
-  const [name, setName] = useState(props.name);
-  const [major, setMajor] = useState(props.major);
+  const [major, setMajor] = useState(user.major!);
   const [advisor, setAdvisor] = useState("");
-  const [email, setEmail] = useState(props.email);
-  const [coop, setCoop] = useState(props.coop);
-  const { majors, plans } = props;
+  const [coopCycle, setCoopCycle] = useState(user.coopCycle);
+
+  const ProfileName = () => {
+    return (
+      <ProfileEntryContainer>
+        <ItemTitle> Name </ItemTitle>
+        <ItemEntry> {user.fullName} </ItemEntry>
+      </ProfileEntryContainer>
+    );
+  };
+
+  const ProfileMajor = () => {
+    const val = !!major ? major : "None Selected";
+    return (
+      <ProfileEntryContainer>
+        <ItemTitle> Major </ItemTitle>
+        {isEdit && (
+          <Autocomplete
+            disableListWrap
+            options={majors.map((maj: { name: any }) => maj.name)}
+            renderInput={params => (
+              <TextField {...params} variant="outlined" fullWidth />
+            )}
+            value={val}
+            onChange={(event: React.SyntheticEvent<{}>, value: any) =>
+              setMajor(value)
+            }
+          />
+        )}
+        {!isEdit && <ItemEntry> {val} </ItemEntry>}
+      </ProfileEntryContainer>
+    );
+  };
+
+  const ProfileCoop = () => {
+    const val = !!coopCycle ? coopCycle : "None Selected";
+    return (
+      <ProfileEntryContainer>
+        <ItemTitle> Co-op Cycle </ItemTitle>
+        {isEdit && (
+          <Autocomplete
+            disableListWrap
+            options={plans[major].map((p: Schedule) => planToString(p))}
+            renderInput={params => (
+              <TextField {...params} variant="outlined" fullWidth />
+            )}
+            value={val}
+            onChange={(event: React.SyntheticEvent<{}>, value: any) =>
+              setCoopCycle(value)
+            }
+          />
+        )}
+        {!isEdit && <ItemEntry> {val} </ItemEntry>}
+      </ProfileEntryContainer>
+    );
+  };
+
+  /*
+  TODO: // Add Advsisors to profile page once we support them
+  const ProfileAdvisor = (props: any) => {
+      return (
+          <ProfileEntryContainer>
+              <ItemTitle> Advisor </ItemTitle>
+              {props.isEdit &&
+                  <Autocomplete
+                      disableListWrap
+                      options={["advisor", "advisor 2", "advisor 3"]}
+                      renderInput={params => (
+                      <TextField
+                          {...params}
+                          variant="outlined"
+                          fullWidth
+                      />
+                      )}
+                      value={props.advisor}
+                      onChange={(event: React.SyntheticEvent<{}>, value: any) => props.setAdvisor(value)}
+                  />
+              }
+              {!props.isEdit &&
+                  <ItemEntry> {props.advisor} </ItemEntry>
+              }
+          </ProfileEntryContainer>
+      );
+  }
+  */
+
+  const ProfileEmail = () => {
+    return (
+      <ProfileEntryContainer>
+        <ItemTitle> Email </ItemTitle>
+        <ItemEntry> {user.email} </ItemEntry>
+      </ProfileEntryContainer>
+    );
+  };
+
+  const save = () => {
+    setEdit(false);
+    dispatch(setUserMajorAction(major));
+    if (coopCycle != undefined && coopCycle !== "None Selected") {
+      dispatch(setUserCoopCycleAction(coopCycle));
+    }
+
+    const token = getAuthToken();
+
+    const updateUserObj: IUpdateUser = {
+      token: token,
+      id: user.id!,
+    };
+
+    const updateUserData: IUpdateUserData = {
+      major: major,
+      coop_cycle:
+        coopCycle != undefined && coopCycle !== "None Selected"
+          ? coopCycle
+          : "",
+    };
+    updateUser(updateUserObj, updateUserData);
+  };
+
+  const SaveButton = () => {
+    return <PrimaryButton onClick={() => save()}>Save</PrimaryButton>;
+  };
+
+  // const ChangePassword = (props: ChangePasswordProps) => {
+  //   const [open, setOpen] = React.useState(false);
+  //   const token = getAuthToken();
+
+  //   return (
+  //     <div>
+  //       <ProfileEntryContainer>
+  //         <ItemTitle> Password </ItemTitle>
+  //         <div onClick={() => setOpen(true)}>
+  //           <OutlinedButton>Change Password</OutlinedButton>
+  //         </div>
+  //       </ProfileEntryContainer>
+  //       <ChangePasswordModal open={open} setOpen={setOpen} token={token} />
+  //     </div>
+  //   );
+  // };
 
   return (
     <Container>
@@ -329,78 +263,22 @@ const ProfileComponent: React.FC = (props: any) => {
           </ProfileTitleContainer>
           <DataContainer>
             <ProfileColumn>
-              {/* <ProfileName isEdit={isEdit} name={name} setName={setName} /> */}
-              <ProfileMajor
-                isEdit={isEdit}
-                major={major == undefined ? "" : major.name}
-                setMajor={setMajor}
-                majors={majors}
-              />
+              <ProfileName />
+              <ProfileMajor />
               {/* {props.token != undefined && props.token.length > 0 && (
                 <ChangePassword token={props.token} id={props.id} />
               )} */}
             </ProfileColumn>
             <ProfileColumn>
-              {/* <ProfileEmail isEdit={isEdit} email={email} setEmail={setEmail} /> */}
-              {major != undefined && (
-                <ProfileCoop
-                  isEdit={isEdit}
-                  coop={coop == undefined ? "" : coop}
-                  setCoop={setCoop}
-                  plans={plans}
-                  major={major}
-                />
-              )}
+              <ProfileEmail />
+              {!!major && <ProfileCoop />}
             </ProfileColumn>
           </DataContainer>
-          {isEdit && (
-            <SaveButton
-              setEdit={setEdit}
-              name={name}
-              email={email}
-              major={major}
-              coop={coop}
-              id={props.id}
-              setDeclaredMajorAction={props.setDeclaredMajorAction}
-              setUserCoopCycleAction={props.setUserCoopCycleAction}
-              setFullNameAction={props.setFullNameAction}
-              setEmailAction={props.setEmailAction}
-            />
-          )}
+          {isEdit && <SaveButton />}
         </InnerContainer>
       </OuterContainer>
     </Container>
   );
 };
 
-/**
- * Callback to be passed into connect, responsible for dispatching redux actions to update the appstate.
- * @param dispatch responsible for dispatching actions to the redux store.
- */
-const mapStateToProps = (state: AppState) => ({
-  major: getUserMajorFromState(state),
-  coop: getUserCoopCycleFromState(state),
-  majors: getMajorsFromState(state),
-  plans: getUserPlansFromState(state),
-  id: getUserIdFromState(state),
-});
-
-/**
- * Callback to be passed into connect, to make properties of the AppState available as this components props.
- * @param state the AppState
- */
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-  setMajor: (major: string) => dispatch(setUserMajorAction(major)),
-  setCoopCycle: (coopCycle: string) =>
-    dispatch(setUserCoopCycleAction(coopCycle)),
-});
-
-/**
- * Convert this React component to a component that's connected to the redux store.
- * When rendering the connecting component, the props assigned in mapStateToProps, do not need to
- * be passed down as props from the parent component.
- */
-export const Profile = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withRouter(ProfileComponent));
+export const Profile = withRouter(ProfileComponent);
