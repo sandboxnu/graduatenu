b58a17cc252b14693eab7e3cbbec7680af75a69a
Andrew Leung <andrewleung104@gmail.com>
2020-12-23 20:41:29 -0500

    run migration

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -18,7 +18,6 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #  nu_id                  :string
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -58,10 +58,10 @@ ActiveRecord::Schema.define(version: 2020_12_21_015048) do
     t.integer "graduation_year"
     t.string "major"
     t.string "coop_cycle"
-    t.integer "catalog_year"
     t.string "image_url"
     t.boolean "is_advisor", default: false, null: false
     t.string "nu_id"
+    t.integer "catalog_year"
     t.json "courses_completed", default: [], array: true
     t.json "courses_transfer", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
