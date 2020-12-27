993b797a857ef237cfa8402bb704ea828d710b2a
Andrew Leung <andrewleung104@gmail.com>
2020-12-27 14:08:34 -0500

    migration with foreign keys

--- api/db/migrate/20201227185349_create_plan_comments.rb
+++ api/db/migrate/20201227185349_create_plan_comments.rb
@@ -1,10 +1,12 @@
 class CreatePlanComments < ActiveRecord::Migration[6.0]
   def change
     create_table :plan_comments do |t|
-      t.bigint :plan_id
-      t.bigint :user_id
-      t.datetime :timestamp
-      t.string :comment
+      t.bigint :plan_id, null: false
+      t.bigint :user_id, null: false
+      t.datetime :timestamp, precision: 6, null: false
+      t.string :comment, null: false
     end
+    add_foreign_key :plan_comments, :users
+    add_foreign_key :plan_comments, :plans
   end
 end
