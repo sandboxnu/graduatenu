bece49d6b34db4270cfcaf9b97134cc1d42e30c7
imanmoreira <moreira.i@husky.neu.edu>
2021-02-17 19:52:18 -0500

    Updating semester to be optional

--- common/types.ts
+++ common/types.ts
@@ -314,7 +314,7 @@ export interface ScheduleCourse {
   coreqs?: INEUAndPrereq | INEUOrPrereq;
   numCreditsMin: number;
   numCreditsMax: number;
-  semester: string | null | undefined;
+  semester?: string | null;
 }
 
 /**
