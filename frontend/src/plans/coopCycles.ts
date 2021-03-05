db2f3f82af090297a305c9bc5c39fff47d602132
Andrew Leung <andrewleung104@gmail.com>
2021-03-04 19:35:06 -0500

    Filter out summer 1 and 2 from coop cycle options

--- frontend/src/plans/coopCycles.ts
+++ frontend/src/plans/coopCycles.ts
@@ -5,27 +5,30 @@ const NUM_YEARS_AND_COOPS_OPTIONS: { numYears: number; numCoops: number }[] = [
   { numYears: 4, numCoops: 2 },
 ];
 
-const SEASON_ENUM_TO_TITLE: Record<SeasonEnum, string> = {
+const SEASON_ENUM_TO_TITLE: Record<string, string> = {
   [SeasonEnum.FL]: "Fall",
   [SeasonEnum.SP]: "Spring",
   [SeasonEnum.SM]: "Summer",
-  [SeasonEnum.S1]: "Summer 1",
-  [SeasonEnum.S2]: "Summer 2",
 };
+
+const isValidCoopCycleSeason = (season: SeasonEnum): boolean =>
+  season !== SeasonEnum.S1 && season !== SeasonEnum.S2;
 
 /**'
  * This is the basic set of co-op cycle options which can be selected for any major.
  * It consists of:
- * - 5 years, 3 co-ops, spring/fall/summer/summer1/summer2
- * - 4 years, 3 co-ops, spring/fall/summer/summer1/summer2
+ * - 5 years, 3 co-ops, spring/fall/summer
+ * - 4 years, 3 co-ops, spring/fall/summer
  */
 export const BASE_FORMATTED_COOP_CYCLES: string[] = NUM_YEARS_AND_COOPS_OPTIONS.map(
   option =>
-    Object.values(SeasonEnum).map((season: SeasonEnum) => {
-      return (
-        `${option.numYears} Years, ` +
-        `${option.numCoops} Co-ops, ` +
-        `${SEASON_ENUM_TO_TITLE[season]} Cycle`
-      );
-    })
+    Object.values(SeasonEnum)
+      .filter(isValidCoopCycleSeason)
+      .map((season: string) => {
+        return (
+          `${option.numYears} Years, ` +
+          `${option.numCoops} Co-ops, ` +
+          `${SEASON_ENUM_TO_TITLE[season]} Cycle`
+        );
+      })
 ).reduce((acc, coopCyclesPerOption) => [...acc, ...coopCyclesPerOption], []);
