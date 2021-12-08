9f82ad220c8d4a8257a065d4d5bf4cbe03ae21bb
Aryan Shah <aryanshah791@gmail.com>
2021-12-07 21:12:54 -0500

    Change to trigger CD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ import { TemplateBuilderPage } from "./advising/Templates/TemplateBuilderPage";
 import { CourseManagmentPage } from "./advising/CourseManagment";
 import { GenericStudentView } from "./advising/ManageStudents/GenericStudentView";
 
+// change
 export const App = ({ store }: { store: Store }) => {
   return (
     <Provider store={store}>
