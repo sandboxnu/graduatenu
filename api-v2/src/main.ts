ca39d70f1ba089079c13b016a5a9b9fb23857716
Aryan Shah <aryanshah791@gmail.com>
2022-01-25 10:01:56 -0500

    Add comments explaining global pipe and interceptor

--- api-v2/src/main.ts
+++ api-v2/src/main.ts
@@ -7,13 +7,23 @@ async function bootstrap() {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
+  /**
+   * Global pipe that ensures all incoming data is validated according to the
+   * class-validator decorators in the incoming data's type.
+   */
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      transform: true,
+      whitelist: true, // strips properties from incoming objects that aren't decorated with class-validator
+      transform: true, // transforms properties from incoming objects to the type they are decorated with by class-validator
     }),
   );
 
+  /**
+   * Global intercerceptor that transforms outgoing data and strips properties
+   * with the "@Exclude" decorator from being sent in the response.
+   * For this to work, returned objects in controllers have to be an actual instance
+   * of the type with the appropriate decorators.
+   */
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   await app.listen(3000);
