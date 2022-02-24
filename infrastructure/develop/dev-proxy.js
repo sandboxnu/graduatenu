aef2da6f4893652de8b08d34d5c2a0dbfc2191e0
Aryan Shah <aryanshah791@gmail.com>
2022-02-24 10:40:40 -0500

    Make new setup backward compatible

--- infrastructure/develop/dev-proxy.js
+++ infrastructure/develop/dev-proxy.js
@@ -3,16 +3,22 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
+/**
+ * A proxy that forwards "/api" requests to the server running on port 3001 and all
+ * other requests to the fronend running on port 3000.
+ */
 const proxy = createProxyMiddleware({
-  target: "http://localhost:3001",
+  target: "http://localhost:3000", // all requests to be routed to frontend running on port 3000
   router: {
-    "/api": "http://localhost:3002",
+    "/api": "http://localhost:3001", // all requests prefixed with "/api" to be routed to backend running on port 3001
   },
   ws: true,
   logLevel: "warn",
 });
 
 app.use("/", proxy);
-app.listen(3000, () => {
+
+// Run the server on port 3002
+app.listen(3002, () => {
   console.log("GraduateNU is up and ready!");
 });
