4f040c8ae14f652fcf48b99aad6203583d8dc379
Aryan Shah <aryanshah791@gmail.com>
2022-02-24 10:18:38 -0500

    Roll back changes

---
+++ frontend/src/services/utils.ts
@@ -0,0 +1,44 @@
+import { ServerErrorResponse, ServerResponse } from "../../../common/types";
+
+export const apiRequest = async <ExpectedData>(
+  path: string,
+  method: RequestInit["method"],
+  body?: any,
+  token?: string, // only required if request needs to be authorized
+  searchParams?: string | Record<string, string> | URLSearchParams
+): Promise<ServerResponse<ExpectedData>> => {
+  // add search params to path
+  const searchParamsAsString = new URLSearchParams(searchParams).toString();
+  const pathWithSearchParams = `${path}?${searchParamsAsString}`;
+
+  // configure headers
+  const headers: RequestInit["headers"] = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  // make the request
+  const response = await fetch(pathWithSearchParams, {
+    method,
+    body: body && JSON.stringify(body),
+    headers,
+  });
+
+  return response.json() as Promise<ServerResponse<ExpectedData>>;
+};
+
+/**
+ * A type guard - whether or not the given ServerResponse is a ServerError.
+ */
+export const isServerError = <T>(
+  response: ServerResponse<T>
+): response is ServerErrorResponse => {
+  if ((response as ServerErrorResponse).error) {
+    return true;
+  }
+
+  return false;
+};
