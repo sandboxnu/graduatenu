51d0262b7d6ac1824b9c47974def548d2a1cff67
Brandon Lim <brandonlim8890@gmail.com>
2022-02-26 17:27:46 -0500

    Add redux store and dispatch states for button

--- frontend/src/components/Schedule/AddSemesterButton.tsx
+++ frontend/src/components/Schedule/AddSemesterButton.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { IPlanData } from "../../models/types";
+import { getActivePlanFromState } from "../../state";
+import { updateActivePlanAction } from "../../state/actions/userPlansActions";
+import { AppState } from "../../state/reducers/state";
 import { WhiteColorButton } from "../common/ColoredButtons";
 
-export const AddSemesterButton = () => {
+type Props = ReduxStoreAddSemesterButtonProps &
+  ReduxDispatchAddSemesterButtonProps;
+
+interface ReduxStoreAddSemesterButtonProps {
+  activePlan: IPlanData;
+}
+
+interface ReduxDispatchAddSemesterButtonProps {
+  addSemester: (newPlan: Partial<IPlanData>) => void;
+}
+
+interface ServiceResponse {
+  data: IPlanData;
+}
+
+const AddSemesterButtonComponent = (props: Props) => {
+  // const handleNewSemester = () => {
+  //   service.addNewSemester().then(({ data }: ServiceResponse) => {
+  //     props.addSemester(data);
+  //   })
+  // }
+
   return (
     <WhiteColorButton
       variant="contained"
@@ -13,8 +40,28 @@ export const AddSemesterButton = () => {
         boxShadow: "0 0 1px rgb(255 255 255 / 50%)",
         borderRadius: "0",
       }}
+      // onClick={handleNewSemester}
     >
       + Add Semester
     </WhiteColorButton>
   );
 };
+
+const mapStateToProps = (state: AppState) => ({
+  activePlan: getActivePlanFromState(state),
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  addSemester: (newPlan: Partial<IPlanData>) =>
+    dispatch(updateActivePlanAction(newPlan)),
+});
+
+export const AddSemesterButton = connect<
+  ReduxStoreAddSemesterButtonProps,
+  ReduxDispatchAddSemesterButtonProps,
+  {},
+  AppState
+>(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddSemesterButtonComponent);
