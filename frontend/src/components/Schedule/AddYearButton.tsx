f88fe1811f61ea5363d9836c775298d2c9ac7593
Jia Mu <mu.ji@northeastern.edu>
2022-03-02 16:40:14 -0500

    fix bug, add disabled button on plan update

--- frontend/src/components/Schedule/AddYearButton.tsx
+++ frontend/src/components/Schedule/AddYearButton.tsx
@@ -2,33 +2,51 @@ import React from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { IPlanData } from "../../models/types";
-import { getActivePlanFromState, getUserIdFromState } from "../../state";
-import { updateActivePlanAction } from "../../state/actions/userPlansActions";
+import {
+  getActivePlanFromState,
+  getIsUpdatingFromState,
+  getUserIdFromState,
+} from "../../state";
+import {
+  updateActivePlanAction,
+  updateActivePlanFetchingAction,
+} from "../../state/actions/userPlansActions";
 import { AppState } from "../../state/reducers/state";
 import { WhiteColorButton } from "../common/ColoredButtons";
 import { addYear } from "../../services/PlanService";
 import { convertPlanToUpdatePlanData } from "../../utils/plan-helpers";
+import { LoadingSpinner } from "../common/LoadingSpinner";
 
 type Props = ReduxStoreAddYearButtonProps & ReduxDispatchAddYearButtonProps;
 
 interface ReduxStoreAddYearButtonProps {
   activePlan: IPlanData;
   userId: number;
+  isUpdating: boolean;
 }
 
 interface ReduxDispatchAddYearButtonProps {
-  addYear: (newPlan: Partial<IPlanData>) => void;
+  updateActivePlan: (newPlan: Partial<IPlanData>) => void;
+  updateActivePlanFetching: () => void;
 }
 
-const AddYearButtonComponent = ({ activePlan, userId }: Props) => {
+const AddYearButtonComponent = ({
+  activePlan,
+  userId,
+  isUpdating,
+  updateActivePlan,
+  updateActivePlanFetching,
+}: Props) => {
   const handleNewSemester = async () => {
+    updateActivePlanFetching();
     const res = await addYear(
       userId,
       activePlan.id,
       convertPlanToUpdatePlanData(activePlan, userId)
     );
+
     // Update store
-    updateActivePlanAction(res.plan);
+    updateActivePlan(res.plan);
   };
 
   return (
@@ -42,6 +60,7 @@ const AddYearButtonComponent = ({ activePlan, userId }: Props) => {
         boxShadow: "0 0 1px rgb(255 255 255 / 50%)",
         borderRadius: "0",
       }}
+      disabled={isUpdating}
       onClick={handleNewSemester}
     >
       + Add Year
@@ -52,11 +71,13 @@ const AddYearButtonComponent = ({ activePlan, userId }: Props) => {
 const mapStateToProps = (state: AppState) => ({
   activePlan: getActivePlanFromState(state),
   userId: getUserIdFromState(state),
+  isUpdating: getIsUpdatingFromState(state),
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
-  addYear: (newPlan: Partial<IPlanData>) =>
+  updateActivePlan: (newPlan: Partial<IPlanData>) =>
     dispatch(updateActivePlanAction(newPlan)),
+  updateActivePlanFetching: () => dispatch(updateActivePlanFetchingAction()),
 });
 
 export const AddYearButton = connect<
