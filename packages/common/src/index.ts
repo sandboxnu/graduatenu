4d613a306e348b4ecdaf7c0029811eb3be3595c9
Jia Mu <jiamu0614@gmail.com>
2022-04-03 16:13:32 -0400

    add dto exports to common index.ts

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -5,4 +5,10 @@ export * from "./ap_exams";
 export * from "./constants";
 export * from "./course-utils";
 
+export * from "./dto/create-plan.dto";
+export * from "./dto/create-student.dto";
+export * from "./dto/login-student.dto";
+export * from "./dto/update-plan.dto";
+export * from "./dto/update-student.dto";
+
 // this file IS included in the build
