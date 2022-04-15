fe671d7fda027cca0f2d5442cc32e34ee41bef9d
Brandon Lim <brandonlim8890@gmail.com>
2022-04-15 12:31:33 -0400

    Revert partial dto-types

--- packages/common/src/dto-types.ts
+++ packages/common/src/dto-types.ts
@@ -11,8 +11,6 @@ import {
 } from "class-validator";
 import { CourseWarning, IWarning, Schedule, ScheduleCourse } from "./types";
 
-// import { PartialType } from "@nestjs/mapped-types";
-
 export class CreatePlanDto {
   @IsString()
   name: string;
@@ -41,7 +39,41 @@ export class CreatePlanDto {
   warnings: IWarning[];
 }
 
-// export class UpdatePlanDto extends PartialType(CreatePlanDto) {}
+export class UpdatePlanDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsObject()
+  schedule?: Schedule;
+
+  @IsOptional()
+  @IsString()
+  major?: string;
+
+  @IsOptional()
+  @IsString()
+  coopCycle?: string;
+
+  @IsOptional()
+  @IsString()
+  concentration?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1898)
+  @Max(3000)
+  catalogYear?: number;
+
+  @IsOptional()
+  @IsArray()
+  courseWarnings?: CourseWarning[];
+
+  @IsOptional()
+  @IsArray()
+  warnings?: IWarning[];
+}
 
 export class CreateStudentDto {
   @IsNotEmpty()
@@ -102,7 +134,68 @@ export class CreateStudentDto {
   concentration?: string;
 }
 
-// export class UpdateStudentDto extends PartialType(CreateStudentDto) {}
+export class UpdateStudentDto {
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  fullName?: string;
+
+  @IsOptional()
+  @IsString()
+  nuid?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1898) // the year NEU was established
+  @Max(3000) // will GraduateNU last beyond year 3000?!
+  academicYear?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1898)
+  @Max(3000)
+  graduateYear?: number;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1898)
+  @Max(3000)
+  catalogYear?: number;
+
+  @IsOptional()
+  @IsString()
+  major?: string;
+
+  @IsOptional()
+  @IsString()
+  coopCycle?: string;
+
+  @IsOptional()
+  @IsObject()
+  coursesCompleted?: ScheduleCourse[];
+
+  @IsOptional()
+  @IsObject()
+  coursesTransfered?: ScheduleCourse[];
+
+  @IsOptional()
+  @IsInt()
+  primaryPlanId?: number;
+
+  @IsOptional()
+  @IsString()
+  concentration?: string;
+}
 
 export class LoginStudentDto {
   @IsNotEmpty()
