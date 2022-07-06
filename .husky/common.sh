acd9498db0ae38527eab83673bafd13fa428b650
Alex Takayama <alex@takayamasan.com>
2022-07-06 14:08:07 -0400

    fix husky

---
+++ .husky/common.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# taken from https://typicode.github.io/husky/#/?id=yarn-on-windows
+
+command_exists () {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Workaround for Windows 10, Git Bash and Yarn
+if command_exists winpty && test -t 1; then
+  exec < /dev/tty
+fi
