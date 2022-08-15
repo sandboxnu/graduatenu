ed9de114f3f5e41b3b75e01f4ece91facb7a4fa3
Aryan Shah <aryanshah@Aryans-MacBook-Pro.local>
2022-08-15 17:15:14 -0400

    Add auth logs

--- packages/api-v2/src/auth/jwt.strategy.ts
+++ packages/api-v2/src/auth/jwt.strategy.ts
@@ -1,23 +1,33 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { Request } from "express";
 import { Strategy } from "passport-jwt";
 import { EnvironmentVariables } from "src/environment-variables";
 import { Student } from "src/student/entities/student.entity";
+import { formatServiceCtx } from "src/utils";
 import { AuthService } from "./auth.service";
 import { JwtPayload } from "./interfaces/jwt-payload";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  private readonly logger: Logger = new Logger();
+
   constructor(
     private readonly authService: AuthService,
     configService: ConfigService<EnvironmentVariables, true>
   ) {
     super({
       // Extract JWT from cookie in requests
-      jwtFromRequest: (request: Request) =>
-        request?.cookies?.auth_cookie ?? null,
+      jwtFromRequest: (request: Request) => {
+        const authCookie = request?.cookies?.auth_cookie;
+        if (!authCookie) {
+          this.logger.debug(
+            { message: "Auth cookie missing" },
+            JwtStrategy.formatJwtStrategyCtx("jwtFromRequest")
+          );
+        }
+      },
 
       // Secret to decode the JWT(same one used to encode it)
       secretOrKey: configService.get("JWT_SECRET_KEY", { infer: true }),
@@ -36,9 +46,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     const student = await this.authService.validateJwtPayload(jwtPayload);
 
     if (!student) {
+      this.logger.debug(
+        { message: "Invalid jwt payload", student },
+        JwtStrategy.formatJwtStrategyCtx("validate")
+      );
       throw new UnauthorizedException();
     }
 
     return student;
+  }
+
+  private static formatJwtStrategyCtx(method: string) {
+    return formatServiceCtx(JwtStrategy.name, method);
   }
 }
