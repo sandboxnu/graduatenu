feeb83cdc8fa04fd7b3b4ee3ab2ba7c8d2889163
Tze Zhe Brandon Lim <brandonlim8890@gmail.com>
2022-10-23 14:39:11 -0400

    fix merge conflicts

--- packages/frontend-v2/components/Plan/index.ts
+++ packages/frontend-v2/components/Plan/index.ts
@@ -1,8 +1,5 @@
 export * from "./Plan";
 export * from "./PlanDropdown";
 export * from "./AddPlanModal";
-<<<<<<< HEAD
 export * from "./EditPlanModal";
-=======
 export * from "./DeletePlanModal";
->>>>>>> dcef4d36 (Add support for deleting a plan)
