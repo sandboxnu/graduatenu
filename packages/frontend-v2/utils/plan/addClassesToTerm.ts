0e7cdc64b1e6a4c163bcfc05485374c52815b9c4
nickmamisashvili <mamisashvili.n@northeastern.edu>
2023-01-26 21:21:42 -0500

    replaced term.year

--- packages/frontend-v2/utils/plan/addClassesToTerm.ts
+++ packages/frontend-v2/utils/plan/addClassesToTerm.ts
@@ -17,18 +17,60 @@ export const addClassesToTerm = (
 ) => {
   const updatedPlan = produce(plan, (draftPlan) => {
     const schedule = draftPlan.schedule;
+    schedule.years;
 
-    // find the term
-    const terms = flattenScheduleToTerms(schedule);
-    const term = terms.find(
-      (term) => term.year === termYear && term.season === termSeason
-    );
+    const scheduleYear = schedule.years.find((year) => termYear === year.year);
 
-    if (!term) {
+    if (!scheduleYear) {
       const errMsg = "Term with given year and season not found.";
       logger.debug("addClassesToTerm", errMsg, termYear, termSeason, plan);
       throw new Error("Term with given year and season not found.");
     }
+
+    let term = undefined;
+
+    switch (termSeason) {
+      case SeasonEnum.FL:
+        term = scheduleYear.fall;
+        break;
+      case SeasonEnum.SP:
+        term = scheduleYear.spring;
+        break;
+      case SeasonEnum.S1:
+        term = scheduleYear.summer1;
+        break;
+      case SeasonEnum.S2:
+        term = scheduleYear.summer2;
+        break;
+      case SeasonEnum.SM:
+        throw new Error(
+          "Full summer doesn't exist right now come back when it exists!"
+        );
+    }
+
+    const seasonEnumToTermSeason = {
+      FL: "fall",
+      SP: "spring",
+      S1: "summer1",
+      S2: "summer2",
+    };
+
+    // const term = scheduleYear[termSeason]
+
+    // find the term
+    // const term = scheduleYear].find(
+    //   (termToCheck) => termToCheck.year === termYear && termToCheck.season === termSeason
+    // );
+
+    // (term) => {
+    //   return term.year === termYear && term.season === termSeason
+    // }
+
+    // function test(term) {
+    //   return term.year === termYear && term.season === termSeason
+    // }
+
+    const terms = flattenScheduleToTerms(schedule);
 
     // populate courses with dnd id
     const courseCount = terms.reduce(
