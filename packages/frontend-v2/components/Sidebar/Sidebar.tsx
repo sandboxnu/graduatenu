8e0d1acfa692fe419ed01993849da42ece1a9494
Dylan Dinio <DDinio2101@gmail.com>
2023-02-05 16:01:37 -0500

    Implemented loading functionality using new hooks

--- packages/frontend-v2/components/Sidebar/Sidebar.tsx
+++ packages/frontend-v2/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
 import { Box, Text } from "@chakra-ui/react";
-import { SearchAPI } from "@graduate/api-client";
 import {
   IRequiredCourse,
   MajorValidationError,
@@ -66,7 +65,7 @@ let currentRequestNum = 0;
 
 const Sidebar: React.FC<SidebarProps> = memo(({ selectedPlan }) => {
   const router = useRouter();
-  const { major, isLoading, error } = useMajor(
+  const { major, isLoading: isMajorLoading, error } = useMajor(
     selectedPlan.catalogYear,
     selectedPlan.major
   );
@@ -140,7 +139,6 @@ const Sidebar: React.FC<SidebarProps> = memo(({ selectedPlan }) => {
   useEffect(() => revalidateMajor(), [selectedPlan, major]);
 
   const [courseData, setCourseData] = useState({});
-  const [loading, setLoading] = useState(true);
 
   
   const getAllCoursesInMajor = ():  { subject: string; classId: string }[]=>{
@@ -179,13 +177,23 @@ const Sidebar: React.FC<SidebarProps> = memo(({ selectedPlan }) => {
     }
   }
 
-  const courses = getAllCoursesInMajor();
+  const majorCourses = getAllCoursesInMajor();
 
-  const fetchCourses = () => {
-    useFetchSearchCourses(courses)
+  const {courses, isLoading: isCoursesLoading} = useFetchSearchCourses(majorCourses)
+
+  const courseMap: { [id: string]: ScheduleCourse2<null> } = courseData;
+  if (courses) {
+    for (const course of courses) {
+      if (course) {
+        courseMap[`${course.subject}${course.classId}`] = course;
+      }
+    }
+    setCourseData(courseMap);
+    if(isCoursesLoading){
+    }
   }
 
-  if (isLoading) {
+  if (isMajorLoading) {
     return <SidebarContainer title="Loading..." />;
   }
 
@@ -239,7 +247,7 @@ const Sidebar: React.FC<SidebarProps> = memo(({ selectedPlan }) => {
                 validationStatus={sectionValidationStatus}
                 courseData={courseData}
                 dndIdPrefix={`${SIDEBAR_DND_ID_PREFIX}-${index}`}
-                loading={loading}
+                loading={isCoursesLoading}
               />
             );
           })}
