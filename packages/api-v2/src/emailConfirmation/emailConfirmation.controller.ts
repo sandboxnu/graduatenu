f9578bd99c52c5aa133c5d1891de366b6c1b77e7
nickmamisashvili <mamisashvili.n@northeastern.edu>
2023-02-16 21:11:33 -0500

    Backend is working !

--- packages/api-v2/src/emailConfirmation/emailConfirmation.controller.ts
+++ packages/api-v2/src/emailConfirmation/emailConfirmation.controller.ts
@@ -1,20 +1,11 @@
-import {
-  Controller,
-  ClassSerializerInterceptor,
-  UseInterceptors,
-  Post,
-  Body,
-  UseGuards,
-  Req,
-} from "@nestjs/common";
+import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
 
 import { ConfirmEmailDto } from "@graduate/common";
-import EmailConfirmationService from "./emailConfirmation.service";
-import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
 import { AuthenticatedRequest } from "src/auth/interfaces/authenticated-request";
+import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
+import EmailConfirmationService from "./emailConfirmation.service";
 
 @Controller("email-confirmation")
-@UseInterceptors(ClassSerializerInterceptor)
 export class EmailConfirmationController {
   constructor(
     private readonly emailConfirmationService: EmailConfirmationService
