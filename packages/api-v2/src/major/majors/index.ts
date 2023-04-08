bdbb8223c79d8aea29640c26d69535d68cbe9a0a
AlpacaFur <luke@bigring.org>
2023-04-08 16:21:12 -0400

    Enable All Majors on Frontend

--- packages/api-v2/src/major/majors/index.ts
+++ packages/api-v2/src/major/majors/index.ts
@@ -24,18 +24,32 @@ import * as Computer_Science_and_History_BS_2021 from "./Computer_Science_and_Hi
 import * as Computer_Science_and_History_BS_2022 from "./Computer_Science_and_History_BS/Computer_Science_and_History_BS-2022.json";
 import * as Computer_Science_and_Journalism_BS_2021 from "./Computer_Science_and_Journalism_BS/Computer_Science_and_Journalism_BS-2021.json";
 import * as Computer_Science_and_Journalism_BS_2022 from "./Computer_Science_and_Journalism_BS/Computer_Science_and_Journalism_BS-2022.json";
+import * as Computer_Science_and_Linguistics_BS_2021 from "./Computer_Science_and_Linguistics_BS/Computer_Science_and_Linguistics_BS-2021.json";
+import * as Computer_Science_and_Linguistics_BS_2022 from "./Computer_Science_and_Linguistics_BS/Computer_Science_and_Linguistics_BS-2022.json";
 import * as Computer_Science_and_Mathematics_BS_2021 from "./Computer_Science_and_Mathematics_BS/Computer_Science_and_Mathematics_BS-2021.json";
 import * as Computer_Science_and_Mathematics_BS_2022 from "./Computer_Science_and_Mathematics_BS/Computer_Science_and_Mathematics_BS-2022.json";
+import * as Computer_Science_and_Media_Arts_BS_2021 from "./Computer_Science_and_Media_Arts_BS/Computer_Science_and_Media_Arts_BS-2021.json";
+import * as Computer_Science_and_Media_Arts_BS_2022 from "./Computer_Science_and_Media_Arts_BS/Computer_Science_and_Media_Arts_BS-2022.json";
 import * as Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2021 from "./Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS/Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS-2021.json";
 import * as Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2022 from "./Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS/Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS-2022.json";
+import * as Computer_Science_and_Philosophy_BS_2021 from "./Computer_Science_and_Philosophy_BS/Computer_Science_and_Philosophy_BS-2021.json";
+import * as Computer_Science_and_Philosophy_BS_2022 from "./Computer_Science_and_Philosophy_BS/Computer_Science_and_Philosophy_BS-2022.json";
 import * as Computer_Science_and_Physics_BS_2021 from "./Computer_Science_and_Physics_BS/Computer_Science_and_Physics_BS-2021.json";
 import * as Computer_Science_and_Physics_BS_2022 from "./Computer_Science_and_Physics_BS/Computer_Science_and_Physics_BS-2022.json";
+import * as Computer_Science_and_Sociology_BS_2021 from "./Computer_Science_and_Sociology_BS/Computer_Science_and_Sociology_BS-2021.json";
+import * as Computer_Science_and_Sociology_BS_2022 from "./Computer_Science_and_Sociology_BS/Computer_Science_and_Sociology_BS-2022.json";
+import * as Computer_Science_and_Theatre_BS_2021 from "./Computer_Science_and_Theatre_BS/Computer_Science_and_Theatre_BS-2021.json";
+import * as Computer_Science_and_Theatre_BS_2022 from "./Computer_Science_and_Theatre_BS/Computer_Science_and_Theatre_BS-2022.json";
 import * as Computer_Science_BACS_2021 from "./Computer_Science_BACS/Computer_Science_BACS-2021.json";
 import * as Computer_Science_BACS_2022 from "./Computer_Science_BACS/Computer_Science_BACS-2022.json";
 import * as Computer_Science_BSCS_2021 from "./Computer_Science_BSCS/Computer_Science_BSCS-2021.json";
 import * as Computer_Science_BSCS_2022 from "./Computer_Science_BSCS/Computer_Science_BSCS-2022.json";
 import * as Cybersecurity_and_Business_Administration_BS_2021 from "./Cybersecurity_and_Business_Administration_BS/Cybersecurity_and_Business_Administration_BS-2021.json";
 import * as Cybersecurity_and_Business_Administration_BS_2022 from "./Cybersecurity_and_Business_Administration_BS/Cybersecurity_and_Business_Administration_BS-2022.json";
+import * as Cybersecurity_and_Criminal_Justice_BS_2021 from "./Cybersecurity_and_Criminal_Justice_BS/Cybersecurity_and_Criminal_Justice_BS-2021.json";
+import * as Cybersecurity_and_Criminal_Justice_BS_2022 from "./Cybersecurity_and_Criminal_Justice_BS/Cybersecurity_and_Criminal_Justice_BS-2022.json";
+import * as Cybersecurity_and_Economics_BS_2021 from "./Cybersecurity_and_Economics_BS/Cybersecurity_and_Economics_BS-2021.json";
+import * as Cybersecurity_and_Economics_BS_2022 from "./Cybersecurity_and_Economics_BS/Cybersecurity_and_Economics_BS-2022.json";
 import * as Cybersecurity_BS_2021 from "./Cybersecurity_BS/Cybersecurity_BS-2021.json";
 import * as Cybersecurity_BS_2022 from "./Cybersecurity_BS/Cybersecurity_BS-2022.json";
 import * as Data_Science_and_Behavioral_Neuroscience_BS_2021 from "./Data_Science_and_Behavioral_Neuroscience_BS/Data_Science_and_Behavioral_Neuroscience_BS-2021.json";
@@ -66,6 +80,10 @@ import * as Data_Science_BS_2021 from "./Data_Science_BS/Data_Science_BS-2021.js
 import * as Data_Science_BS_2022 from "./Data_Science_BS/Data_Science_BS-2022.json";
 import * as Game_Art_and_Animation_BFA_2021 from "./Game_Art_and_Animation_BFA/Game_Art_and_Animation_BFA-2021.json";
 import * as Game_Art_and_Animation_BFA_2022 from "./Game_Art_and_Animation_BFA/Game_Art_and_Animation_BFA-2022.json";
+import * as Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2021 from "./Game_Design_and_Music_with_concentration_in_Music_Technology_BS/Game_Design_and_Music_with_concentration_in_Music_Technology_BS-2021.json";
+import * as Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2022 from "./Game_Design_and_Music_with_concentration_in_Music_Technology_BS/Game_Design_and_Music_with_concentration_in_Music_Technology_BS-2022.json";
+import * as Game_Design_BFA_2021 from "./Game_Design_BFA/Game_Design_BFA-2021.json";
+import * as Game_Design_BFA_2022 from "./Game_Design_BFA/Game_Design_BFA-2022.json";
 
 const SUPPORTED_MAJORS_2021: Record<string, Major2> = {
   [Computer_Science_and_Behavioral_Neuroscience_BS_2021.name]:
@@ -92,16 +110,30 @@ const SUPPORTED_MAJORS_2021: Record<string, Major2> = {
     Computer_Science_and_History_BS_2021 as any,
   [Computer_Science_and_Journalism_BS_2021.name]:
     Computer_Science_and_Journalism_BS_2021 as any,
+  [Computer_Science_and_Linguistics_BS_2021.name]:
+    Computer_Science_and_Linguistics_BS_2021 as any,
   [Computer_Science_and_Mathematics_BS_2021.name]:
     Computer_Science_and_Mathematics_BS_2021 as any,
+  [Computer_Science_and_Media_Arts_BS_2021.name]:
+    Computer_Science_and_Media_Arts_BS_2021 as any,
   [Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2021.name]:
     Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2021 as any,
+  [Computer_Science_and_Philosophy_BS_2021.name]:
+    Computer_Science_and_Philosophy_BS_2021 as any,
   [Computer_Science_and_Physics_BS_2021.name]:
     Computer_Science_and_Physics_BS_2021 as any,
+  [Computer_Science_and_Sociology_BS_2021.name]:
+    Computer_Science_and_Sociology_BS_2021 as any,
+  [Computer_Science_and_Theatre_BS_2021.name]:
+    Computer_Science_and_Theatre_BS_2021 as any,
   [Computer_Science_BACS_2021.name]: Computer_Science_BACS_2021 as any,
   [Computer_Science_BSCS_2021.name]: Computer_Science_BSCS_2021 as any,
   [Cybersecurity_and_Business_Administration_BS_2021.name]:
     Cybersecurity_and_Business_Administration_BS_2021 as any,
+  [Cybersecurity_and_Criminal_Justice_BS_2021.name]:
+    Cybersecurity_and_Criminal_Justice_BS_2021 as any,
+  [Cybersecurity_and_Economics_BS_2021.name]:
+    Cybersecurity_and_Economics_BS_2021 as any,
   [Cybersecurity_BS_2021.name]: Cybersecurity_BS_2021 as any,
   [Data_Science_and_Behavioral_Neuroscience_BS_2021.name]:
     Data_Science_and_Behavioral_Neuroscience_BS_2021 as any,
@@ -130,6 +162,9 @@ const SUPPORTED_MAJORS_2021: Record<string, Major2> = {
   [Data_Science_BS_2021.name]: Data_Science_BS_2021 as any,
   [Game_Art_and_Animation_BFA_2021.name]:
     Game_Art_and_Animation_BFA_2021 as any,
+  [Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2021.name]:
+    Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2021 as any,
+  [Game_Design_BFA_2021.name]: Game_Design_BFA_2021 as any,
 };
 
 const SUPPORTED_MAJORS_2022: Record<string, Major2> = {
@@ -157,16 +192,30 @@ const SUPPORTED_MAJORS_2022: Record<string, Major2> = {
     Computer_Science_and_History_BS_2022 as any,
   [Computer_Science_and_Journalism_BS_2022.name]:
     Computer_Science_and_Journalism_BS_2022 as any,
+  [Computer_Science_and_Linguistics_BS_2022.name]:
+    Computer_Science_and_Linguistics_BS_2022 as any,
   [Computer_Science_and_Mathematics_BS_2022.name]:
     Computer_Science_and_Mathematics_BS_2022 as any,
+  [Computer_Science_and_Media_Arts_BS_2022.name]:
+    Computer_Science_and_Media_Arts_BS_2022 as any,
   [Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2022.name]:
     Computer_Science_and_Music_with_Concentration_in_Music_Technology_BS_2022 as any,
+  [Computer_Science_and_Philosophy_BS_2022.name]:
+    Computer_Science_and_Philosophy_BS_2022 as any,
   [Computer_Science_and_Physics_BS_2022.name]:
     Computer_Science_and_Physics_BS_2022 as any,
+  [Computer_Science_and_Sociology_BS_2022.name]:
+    Computer_Science_and_Sociology_BS_2022 as any,
+  [Computer_Science_and_Theatre_BS_2022.name]:
+    Computer_Science_and_Theatre_BS_2022 as any,
   [Computer_Science_BACS_2022.name]: Computer_Science_BACS_2022 as any,
   [Computer_Science_BSCS_2022.name]: Computer_Science_BSCS_2022 as any,
   [Cybersecurity_and_Business_Administration_BS_2022.name]:
     Cybersecurity_and_Business_Administration_BS_2022 as any,
+  [Cybersecurity_and_Criminal_Justice_BS_2022.name]:
+    Cybersecurity_and_Criminal_Justice_BS_2022 as any,
+  [Cybersecurity_and_Economics_BS_2022.name]:
+    Cybersecurity_and_Economics_BS_2022 as any,
   [Cybersecurity_BS_2022.name]: Cybersecurity_BS_2022 as any,
   [Data_Science_and_Behavioral_Neuroscience_BS_2022.name]:
     Data_Science_and_Behavioral_Neuroscience_BS_2022 as any,
@@ -195,6 +244,9 @@ const SUPPORTED_MAJORS_2022: Record<string, Major2> = {
   [Data_Science_BS_2022.name]: Data_Science_BS_2022 as any,
   [Game_Art_and_Animation_BFA_2022.name]:
     Game_Art_and_Animation_BFA_2022 as any,
+  [Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2022.name]:
+    Game_Design_and_Music_with_concentration_in_Music_Technology_BS_2022 as any,
+  [Game_Design_BFA_2022.name]: Game_Design_BFA_2022 as any,
 };
 
 const SUPPORED_MAJORS_NAMES_2021 = Object.keys(SUPPORTED_MAJORS_2021);
