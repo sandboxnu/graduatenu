8b3876b58b1a8cef2da35ad025067d81e8974039
Luke Taylor <taylor.luk@northeastern.edu>
2023-11-05 13:37:07 -0500

    Move Scraper Code out of Repo (#647)

--- packages/scrapers-v2/src/classify/types.ts
+++
@@ -1,8 +0,0 @@
-export enum CatalogEntryType {
-  Major = "Major",
-  Minor = "Minor",
-  Concentration = "Concentration",
-  Unknown = "Unknown",
-}
-
-export type TypedCatalogEntry = { url: URL; type: CatalogEntryType };
