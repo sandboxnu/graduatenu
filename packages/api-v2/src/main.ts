ee976542445544a4665c1cb14ddc6269aa59ed26
lukejianu <71729102+lukejianu@users.noreply.github.com>
2023-11-17 20:50:07 -0500

    fix: add methods to cors

--- packages/api-v2/src/main.ts
+++ packages/api-v2/src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     cors: {
       origin:
         "https://graduatenu-frontend-v2-git-christina-move-fro-b625a5-sandboxneu.vercel.app",
+      methods: ["GET", "PUT", "POST", "PATCH", "DELETE"],
     },
   });
 
