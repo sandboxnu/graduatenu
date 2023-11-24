46527137a58b9df5de1c6ff9a88e4c1303030ea1
Luke Taylor <luke@bigring.org>
2023-11-24 17:31:35 -0500

    feat: Improve deploy script CLI APIs

--- infrastructure/prod/get-ecr-image-name.sh
+++ infrastructure/prod/get-ecr-image-name.sh
@@ -1,23 +1,33 @@
-if [[ ! " backend frontend " =~ " $1 " ]]; then
-  echo "Please provide ECR repo to use: backend or frontend"
+if [[ $# != 2 ]]; then
+  echo "Usage: get-ecr-image-name.sh <COMMIT_TAG | latest-main | local-head> <frontend | backend>"
   exit 1
 fi
 
-CURRENT_HASH=$( git rev-parse HEAD )
-
-if [[ $1 = "backend" ]]; then
-  REPO="graduatenu-rails"
+if [[ ${#1} = 40 ]]; then
+  ECR_IMAGE_COMMIT_HASH=$1
+elif [[ $1 = "latest-main" ]]; then
+  ECR_IMAGE_COMMIT_HASH=$(git ls-remote https://github.com/sandboxnu/graduatenu.git main | awk '{ print $1 }')
+elif [[ $1 = "local-head" ]]; then
+  ECR_IMAGE_COMMIT_HASH=$(git rev-parse HEAD)
+else
+  echo "ERROR: Invalid commit '$1'"
+  exit 1
 fi
 
-if [[ $1 = "frontend" ]]; then
+if [[ $2 = "frontend" ]]; then
   REPO="graduatenu-node"
+elif [[ $2 = "backend" ]]; then
+  REPO="graduatenu-rails"
+else
+  echo "Please choose a service to deploy: 'frontend' or 'backend'"
+  exit 1
 fi
 
-AWS_DEFAULT_REGION="us-east-1"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_DEFAULT_REGION="us-east-1"
 
 ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
 
-ECR_IMAGE_NAME="${ECR_REGISTRY}/${REPO}:${CURRENT_HASH}"
+ECR_IMAGE_NAME="${ECR_REGISTRY}/${REPO}:${ECR_IMAGE_COMMIT_HASH}"
 
 echo $ECR_IMAGE_NAME
--- infrastructure/prod/redeploy.sh
+++ infrastructure/prod/redeploy.sh
@@ -1,54 +1,63 @@
 #!/bin/bash
 
+if [[ $# != 3 ]]; then
+  echo "Usage: redeploy.sh <prod | staging> <COMMIT_TAG | latest-main | local-head> <frontend | backend | both>"
+  exit 1
+fi
+
 if [[ ! " prod staging " =~ " $1 " ]]; then
   echo "Please provide environment to use: prod or staging"
   exit 1
 fi
 
+if [[ ${#2} = 40 ]]; then
+  ECR_IMAGE_COMMIT_HASH=$2
+elif [[ $2 = "latest-main" ]]; then
+  ECR_IMAGE_COMMIT_HASH=$(git ls-remote https://github.com/sandboxnu/graduatenu.git main | awk '{ print $1 }')
+elif [[ $2 = "local-head" ]]; then
+  ECR_IMAGE_COMMIT_HASH=$(git rev-parse HEAD)
+else
+  echo "ERROR: Invalid commit '$2'"
+  exit 1
+fi
+
+if [[ $3 = "frontend" ]]; then
+  DEPLOY_INDEXES=(1)
+elif [[ $3 = "backend" ]]; then
+  DEPLOY_INDEXES=(0)
+elif [[ $3 = "both" ]]; then
+  DEPLOY_INDEXES=(0 1)
+else
+  echo "Please choose a service to deploy: 'frontend', 'backend', or 'both'"
+  exit 1
+fi
+
+echo "Deploying $3 repo(s) to $1 with commit "$2" ($ECR_IMAGE_COMMIT_HASH)"
+
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_DEFAULT_REGION="us-east-1"
 REPOS=( "graduatenu-rails" "graduatenu-node" )
-CURRENT_HASH=$(git ls-remote https://github.com/sandboxnu/graduatenu.git main | awk '{ print $1 }')
 ECS_CLUSTER="$1-graduatenu"
 TASK_FAMILIES=( "${ECS_CLUSTER}-api" "${ECS_CLUSTER}-web" )
 SERVICES=( "${ECS_CLUSTER}-api" "${ECS_CLUSTER}-web" )
 
-INDEXES=(0 1)
-
-if [[ "frontend" = $2 ]]; then
-  echo "INFO: Deploying frontend only."
-  INDEXES=(1)
-fi
-
-if [[ "backend" = $2 ]]; then
-  echo "INFO: Deploying backend only."
-  INDEXES=(0)
-fi
-
-if [[ "current" = $3 ]]; then
-  echo "INFO: Using current hash!"
-  CURRENT_HASH=$(git rev-parse HEAD)
-fi
 
 # Disable aws from sending stdout to less
 export AWS_PAGER=""
 
-echo "Redeploying services for cluster: ${ECS_CLUSTER} with last pushed image"
-
-
-for ii in "${!INDEXES[@]}"; do
-    echo "Deploying ${INDEXES[ii]}..."
-    i=${INDEXES[ii]}
+for service_index in "${!DEPLOY_INDEXES[@]}"; do
+    echo "Deploying ${REPOS[DEPLOY_INDEXES[service_index]]}..."
+    i=${DEPLOY_INDEXES[service_index]}
     # Last pushed image should always be tagged with the latest commit hash on main 
-    ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOS[$i]}:${CURRENT_HASH}"
+    ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${REPOS[$i]}:${ECR_IMAGE_COMMIT_HASH}"
     TASK_FAMILY="${TASK_FAMILIES[$i]}"
     SERVICE="${SERVICES[$i]}"
     # fetch template for task definition 
     TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "$TASK_FAMILY" --region "$AWS_DEFAULT_REGION")
     # update the template's image to use the latest ECR_IMAGE
-    NEW_TASK_DEFINTIION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
+    NEW_TASK_DEFINITION=$(echo $TASK_DEFINITION | jq --arg IMAGE "$ECR_IMAGE" '.taskDefinition | .containerDefinitions[0].image = $IMAGE | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.requiresAttributes) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)')
     # register the new revision for the task definition 
-    NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINTIION")
+    NEW_TASK_INFO=$(aws ecs register-task-definition --region "$AWS_DEFAULT_REGION" --cli-input-json "$NEW_TASK_DEFINITION")
     NEW_REVISION=$(echo $NEW_TASK_INFO | jq '.taskDefinition.revision')
     # update the service to replace tasks with the latest revision using the latest image
     aws ecs update-service --cluster ${ECS_CLUSTER} \
