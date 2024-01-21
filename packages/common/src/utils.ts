8c7ba4aeccff4fc67330625051b2eb6a04e8f93f
Kobe <kobezhe@gmail.com>
2024-01-21 18:36:29 -0500

    util to update supported major sort method

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -3,3 +3,19 @@ export const isStrongPassword = (password: string): boolean => {
   const containsLettersAndNumbersRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])/;
   return password.length >= 8 && containsLettersAndNumbersRegex.test(password);
 };
+
+/**
+ * Comparator function for sorting majors by name Criteria: ignores spacing and
+ * special characters when sorting
+ */
+export const majorNameComparator = (a: string, b: string) => {
+  const trimmedA = a
+    .replace(/[^A-Z0-9]/gi, "")
+    .trim()
+    .toLowerCase();
+  const trimmedB = b
+    .replace(/[^A-Z0-9]/gi, "")
+    .trim()
+    .toLowerCase();
+  return trimmedB.localeCompare(trimmedA);
+};
