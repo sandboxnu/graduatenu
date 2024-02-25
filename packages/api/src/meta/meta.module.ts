7bc226d49b25bab4ece8048228574bd9d3e403d8
Daisy Kucharski <83257720+daisykucharski@users.noreply.github.com>
2024-02-25 14:57:49 -0500

    Add health endpoint (#706)
    * health endpoint
    
    * remove my types

--- packages/api/src/meta/meta.module.ts
+++ packages/api/src/meta/meta.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TerminusModule } from "@nestjs/terminus";
+import { HttpModule } from "@nestjs/axios";
 import { MetaService } from "./meta.service";
 import { MetaController } from "./meta.controller";
 
 @Module({
   controllers: [MetaController],
   providers: [MetaService],
+  imports: [HttpModule, TerminusModule],
   exports: [MetaService],
 })
 export class MetaModule {}
