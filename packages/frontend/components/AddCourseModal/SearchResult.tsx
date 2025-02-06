441266f7665e868c798586dd7be061a993238b7d
liamcarvajal <carvajal.l@northeastern.edu>
2025-02-06 15:43:43 -0500

    idk

--- packages/frontend/components/AddCourseModal/SearchResult.tsx
+++ packages/frontend/components/AddCourseModal/SearchResult.tsx
@@ -44,97 +44,94 @@ export const SearchResult: React.FC<SearchResultProps> = ({
   const [opened, setOpened] = useState(false);
 
   return (
-    <Flex
-      justifyContent="space-between"
-      alignItems="end"
-      padding="2xs"
-      paddingY="xs"
-      borderBottom="1px"
-      borderColor="neutral.100"
-    >
-      <Flex width="100%" mr="md" alignItems="center" minH="25px">
-        <Box lineHeight="1.2">
-          <Text as="span" fontSize="sm" fontWeight="bold" marginRight="sm">
-            {getCourseDisplayString(course)}
-          </Text>
-          <Text as="span" fontSize="sm">
-            {course.name}
-          </Text>
-          {course.coreqs &&
-            (opened ? (
-              <ChevronUpIcon
-                boxSize="25px"
-                color="black"
-                onClick={() => setOpened(!opened)}
-              />
-            ) : (
-              <ChevronDownIcon
-                boxSize="25px"
-                color="black"
-                onClick={() => setOpened(!opened)}
-              />
-            ))}
-        </Box>
-        {course.coreqs && (
-          <Collapse in={opened} animateOpacity>
-            <Box
-              px="sm"
-              py="xs"
-              borderRadius="lg"
-              backgroundColor="transparent"
-            >
-              <Text fontSize="sm">boo</Text>
-            </Box>
-          </Collapse>
-        )}
-        <Box ml="auto" mr="sm"></Box>
-        <GraduateToolTip
-          label={`Search for ${getCourseDisplayString(course)} on SearchNEU`}
-        >
-          <a
-            href={getSearchLink(year ?? 2022, season, course)}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <IconButton
-              aria-label="Search course information"
-              icon={<InfoOutlineIcon />}
-              color="primary.blue.light.main"
-              border={0}
-              colorScheme="primary.blue.light.main"
-              isRound
-              size="md"
-              isLoading={isSelectingAnotherCourse}
-              isDisabled={isResultAlreadyAdded || isResultAlreadySelected}
-              alignSelf="center"
-            />
-          </a>
-        </GraduateToolTip>
-        <NUPathLabel
-          nuPaths={course.nupaths ? course.nupaths : []}
-          filteredPaths={filteredPaths}
-        />
-      </Flex>
-
-      <GraduateToolTip
-        label={addButtonTooltip}
-        isDisabled={!isAddButtonDisabled}
+    <Box>
+      <Flex
+        justifyContent="space-between"
+        alignItems="end"
+        padding="2xs"
+        paddingY="xs"
+        borderBottom="1px"
+        borderColor="neutral.100"
       >
-        <IconButton
-          aria-label="Add class"
-          icon={<AddIcon />}
-          color="primary.blue.light.main"
-          borderColor="primary.blue.light.main"
-          colorScheme="primary.blue.light.main"
-          isRound
-          size="xs"
-          mr="sm"
-          onClick={() => addSelectedCourse(course)}
-          isLoading={isSelectingAnotherCourse}
-          isDisabled={isResultAlreadyAdded || isResultAlreadySelected}
-          alignSelf="center"
-        />
-      </GraduateToolTip>
-    </Flex>
+        <Flex width="100%" mr="md" alignItems="center" minH="25px">
+          <Box lineHeight="1.2">
+            <Text as="span" fontSize="sm" fontWeight="bold" marginRight="sm">
+              {getCourseDisplayString(course)}
+            </Text>
+            <Text as="span" fontSize="sm">
+              {course.name}
+            </Text>
+            {course.coreqs &&
+              (opened ? (
+                <ChevronUpIcon
+                  boxSize="25px"
+                  color="black"
+                  onClick={() => setOpened(!opened)}
+                />
+              ) : (
+                <ChevronDownIcon
+                  boxSize="25px"
+                  color="black"
+                  onClick={() => setOpened(!opened)}
+                />
+              ))}
+          </Box>
+          <Box ml="auto" mr="sm"></Box>
+          <GraduateToolTip
+            label={`Search for ${getCourseDisplayString(course)} on SearchNEU`}
+          >
+            <a
+              href={getSearchLink(year ?? 2022, season, course)}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <IconButton
+                aria-label="Search course information"
+                icon={<InfoOutlineIcon />}
+                color="primary.blue.light.main"
+                border={0}
+                colorScheme="primary.blue.light.main"
+                isRound
+                size="md"
+                isLoading={isSelectingAnotherCourse}
+                isDisabled={isResultAlreadyAdded || isResultAlreadySelected}
+                alignSelf="center"
+              />
+            </a>
+          </GraduateToolTip>
+          <NUPathLabel
+            nuPaths={course.nupaths ? course.nupaths : []}
+            filteredPaths={filteredPaths}
+          />
+        </Flex>
+
+        <GraduateToolTip
+          label={addButtonTooltip}
+          isDisabled={!isAddButtonDisabled}
+        >
+          <IconButton
+            aria-label="Add class"
+            icon={<AddIcon />}
+            color="primary.blue.light.main"
+            borderColor="primary.blue.light.main"
+            colorScheme="primary.blue.light.main"
+            isRound
+            size="xs"
+            mr="sm"
+            onClick={() => addSelectedCourse(course)}
+            isLoading={isSelectingAnotherCourse}
+            isDisabled={isResultAlreadyAdded || isResultAlreadySelected}
+            alignSelf="center"
+          />
+        </GraduateToolTip>
+      </Flex>
+      {course.coreqs && (
+        <Collapse in={opened} animateOpacity>
+          <Box px="sm" py="xs" borderRadius="lg" backgroundColor="transparent">
+            <Text fontSize="sm">boo</Text>
+          </Box>
+        </Collapse>
+      )}
+    </Box>
   );
 };
