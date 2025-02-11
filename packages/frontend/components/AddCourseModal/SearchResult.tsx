db0781ffcdecf72c456bf82aee361f1c2b2cac02
Ameeka-Patel <patel.ameek@northeastern.edu>
2025-02-11 18:49:14 -0500

    filter by 4 credits
    only include coreqs for classes that have 4 or more credits

--- packages/frontend/components/AddCourseModal/SearchResult.tsx
+++ packages/frontend/components/AddCourseModal/SearchResult.tsx
@@ -128,6 +128,7 @@ export const SearchResult: React.FC<SearchResultProps> = ({
         </GraduateToolTip>
       </Flex>
       {/* {course.coreqs && (
+      {/* (
         <Collapse in={opened} animateOpacity>
           <Box px="sm" py="xs" borderRadius="lg" backgroundColor="transparent">
             <Text fontSize="sm">boo</Text>
