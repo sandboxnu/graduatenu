36d6d0ec9844c223ff90bf41f280029b705413c3
ShireenKumar <shireen.kumar1304@gmail.com>
2025-02-11 19:16:57 -0500

    working on minor validation:

--- packages/common/src/major2-validation.ts
+++ packages/common/src/major2-validation.ts
@@ -14,6 +14,7 @@ import {
   Result,
   Err,
   Ok,
+  Minor,
 } from "./types";
 import { assertUnreachable, courseToString } from "./course-utils";
 
@@ -283,9 +284,11 @@ export type MajorValidationResult = Result<
 export function validateMajor2(
   major: Major2,
   taken: ScheduleCourse2<unknown>[],
+  minor?: Minor,
   concentrations?: SelectedConcentrationsType
 ): MajorValidationResult {
   const tracker = new Major2ValidationTracker(taken);
+
   let concentrationReq: Requirement2[] = [];
   if (major.concentrations) {
     concentrationReq = getConcentrationsRequirement(
@@ -294,7 +297,19 @@ export function validateMajor2(
     );
   }
 
-  const majorReqs = [...major.requirementSections, ...concentrationReq];
+  let minorRequirements: Requirement2[] = [];
+  if (minor) {
+    // Get the minor requirements and assign them
+    minorRequirements = getMinorRequirement(minor);
+    // console.log(minorRequirements)
+  }
+
+  //const majorReqs = [...major.requirementSections, ...(minor?.requirementSections ?? []), ...concentrationReq];
+  const majorReqs = [
+    ...major.requirementSections,
+    ...minorRequirements,
+    ...concentrationReq,
+  ];
   // create a big AND requirement of all the sections and selected concentrations
   const requirementsResult = validateRequirement(
     {
@@ -304,7 +319,7 @@ export function validateMajor2(
     tracker
   );
   const creditsResult = validateTotalCreditsRequired(
-    major.totalCreditsRequired,
+    major.totalCreditsRequired + (minor?.totalCreditsRequired ?? 0),
     taken
   );
 
@@ -317,6 +332,7 @@ export function validateMajor2(
   }
   const totalCreditsRequirementError =
     creditsResult.type === ResultType.Ok ? undefined : creditsResult.err;
+  console.log("error");
   return Err({
     majorRequirementsError,
     totalCreditsRequirementError,
@@ -375,6 +391,46 @@ function convertToConcentrationsArray(
     return [concentrations];
   }
   return concentrations;
+}
+
+export function getMinorRequirement(
+  inputMinor: undefined | Minor
+): Requirement2[] {
+  // No minor
+  if (!inputMinor) {
+    return [];
+  }
+
+  // put all the minor requirments into minor requirments and assigning type as section
+  const minorRequirements: Section[] = inputMinor.requirementSections;
+
+  const selectedMinors = convertToMinorsArray(inputMinor);
+
+  const matchedRequirements = [];
+  // for (const minor of selectedMinors) {
+  //   console.log(selectedMinors);
+  //   const found = minorRequirements.find((m) => {
+  //     // for(const req of )
+  //     console.log(`Comparing title: ${m.title} with minor name: ${minor.name}`);
+  //     return m.title === minor.name;
+  //   });
+  //   if (!found) {
+  //     throw new Error (`Minor was not found: ${minor.name}`);
+  //   }
+  //   matchedRequirements.push(found);
+  // }
+
+  return [{ type: "AND", courses: minorRequirements }];
+}
+
+function convertToMinorsArray(minors: undefined | Minor | Minor[]): Minor[] {
+  if (minors === undefined) {
+    return [];
+  }
+  if (!Array.isArray(minors)) {
+    return [minors];
+  }
+  return minors;
 }
 
 // the solutions returned may have duplicate courses, indicating the # of times a course is taken
