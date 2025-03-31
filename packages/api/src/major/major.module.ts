e49218833d510f458713ea6d62ae9658aa6e6cb9
Dennis Wang <66754085+denniwang@users.noreply.github.com>
2025-03-31 11:39:33 -0400

    removed template controller from major module

--- packages/api/src/major/major.module.ts
+++ packages/api/src/major/major.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
 import { MajorService } from "./major.service";
 import { MajorController } from "./major.controller";
-import { TemplateController } from "./template.controller";
 
 @Module({
-  controllers: [MajorController, TemplateController],
+  controllers: [MajorController],
   providers: [MajorService],
   exports: [MajorService],
 })
