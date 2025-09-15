d2c6bee1b5c058179b1237de735e43a46e8f28bf
Jake Langlois <langlois.j@northeastern.edu>
2025-09-14 22:00:31 -0400

    fixing useminor hook

--- packages/frontend/hooks/useMinor.ts
+++ packages/frontend/hooks/useMinor.ts
@@ -13,16 +13,14 @@ export function useMinor(
   catalogYear: number,
   minorNames?: string[]
 ): MinorReturn {
-  if (!minorNames) {
-    minorNames = [];
-  }
+  const safeMinorNames = minorNames || [];
   const key =
-    minorNames.length > 0
-      ? `api/minors/${catalogYear}/${minorNames?.join(",")}`
+    safeMinorNames.length > 0
+      ? `api/minors/${catalogYear}/${safeMinorNames.join(",")}`
       : null;
 
   const { data, error } = useSWR(key, async () => {
-    const promises = minorNames?.map((minorName) =>
+    const promises = safeMinorNames.map((minorName) =>
       API.minors.get(catalogYear, minorName)
     );
     return Promise.all(promises);
